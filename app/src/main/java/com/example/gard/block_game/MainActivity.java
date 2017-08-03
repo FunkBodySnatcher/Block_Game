1c7bfc3e09dc933cd7b823a77b4df81a2eb8c3c9
FunkBodySnatcher <30548748+FunkBodySnatcher@users.noreply.github.com>
2017-08-03 15:33:40 +0200

    Hide bars. Text and corresponding method.

--- app/src/main/java/com/example/gard/block_game/MainActivity.java
+++ app/src/main/java/com/example/gard/block_game/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.gard.block_game;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Define text
+    TextView startText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Hide actionbar. May produce nullPointer....
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //Hide status bar.
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        //Get text
+        startText = (TextView) findViewById(R.id.startTextView);
+
     }
+
+    public void startGame(View view){
+
+        startText.setText("");
+
+    }
+
 }
